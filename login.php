743e6b288c8ec121305c3708bfc9c12c6714071a
Jenina Angelin D <jeninaangelind@gmail.com>
2023-07-16 00:01:07 +0530

    Login page for the platform
    Contains options to decide which function should be done

---
+++ login.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+<title> login form</title>
+</head>
+<body> <marquee> <FONT SIZE="+6" COLOR="#0000CC"> LIBRARY MANAGEMENT SYSTEM</FONT> </marquee> 
+ <center>  <br/> <br/> <br/> <br/> <br/> <br/>
+<a href="bookmaster.php">BOOK MASTER </a> &nbsp &nbsp &nbsp
+<a href="membermaster.php">MEMBER MASTER </a> &nbsp &nbsp &nbsp
+<a href="transaction_return.php">BOOK RETURN </a>&nbsp &nbsp &nbsp
+<a href="transaction_issue.php">BOOK ISSUE </a><br/> <br/> <br/>
+<a href="ManageBook.php"> MANAGE BOOK DETAILS</a> &nbsp &nbsp &nbsp
+<a href="ManageMember.php"> MANAGE MEMBER DETAILS </a>
+ <br/> <br/> <br/>
+</body>
+<html>
